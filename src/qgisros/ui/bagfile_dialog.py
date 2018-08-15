312f49884f7965e22396bd5359efcbb73fe0f62c
Andrew Blakey <ablakey@locusrobotics.com>
2018-08-15 15:08:15 -0400

    working and simple, but ugly threading for long load time

--- src/qgisros/ui/bagfile_dialog.py
+++ src/qgisros/ui/bagfile_dialog.py
@@ -1,29 +1,33 @@
 from pathlib import Path
 import os
+import threading
 
 from PyQt5 import uic
 from PyQt5.QtWidgets import QDialog, QFileDialog
+from PyQt5.QtCore import pyqtSignal, pyqtSlot
 
 from qgis.core import QgsProject
 
 from ..core import helpers, TranslatorRegistry
-from .loading_modal import LoadingModal
 
 FORM_CLASS, _ = uic.loadUiType(str(Path(os.path.dirname(__file__)) / 'bagfile_dialog.ui'))
 
 
 class BagfileDialog(QDialog, FORM_CLASS):
 
+    layerCreated = pyqtSignal(object)
+
     def __init__(self, parent=None):
         super(BagfileDialog, self).__init__(parent)
         self.setupUi(self)
 
         self.openBagButton.clicked.connect(self._getBagFileTopics)
-        self.AddTopicButton.clicked.connect(self._createLayerFromSelected)
+        self.addTopicButton.clicked.connect(self._createLayerFromSelected)
         self.unloadBagButton.clicked.connect(self._unloadBag)
 
+        self.layerCreated.connect(self.addCreatedLayer)
+
         self._bagFilePath = None
-        self._loadingModal = LoadingModal(self)  # self as parent.
 
     def _getBagFileTopics(self):
         # Load topic metadata from bag.
@@ -37,13 +41,25 @@ class BagfileDialog(QDialog, FORM_CLASS):
         self.stackedWidget.setCurrentWidget(self.dataLoaderWidgetContainer)
 
     def _createLayerFromSelected(self):
+        self.addTopicButton.setText('Loading...')
+        self.addTopicButton.setEnabled(False)
+
+        t = threading.Thread(name='my_worker', target=self._createLayerWorker)
+        t.start()
+
+    def _createLayerWorker(self):
         name, topicType = self.dataLoaderWidget.getSelectedTopic()
-        self._loadingModal.show()
         messages = helpers.getBagData(self._bagFilePath, name)
         translator = TranslatorRegistry.instance().get(topicType)
         layer = translator.createLayer(name, rosMessages=messages)
+        self.layerCreated.emit(layer)
+
+        self.addTopicButton.setText('Add Layer')
+        self.addTopicButton.setEnabled(True)
+
+    @pyqtSlot(object)
+    def addCreatedLayer(self, layer):
         QgsProject.instance().addMapLayer(layer)
-        self._loadingModal.hide()
 
     def _unloadBag(self):
         '''Frees up any resources from the bag, resets view and state.'''
